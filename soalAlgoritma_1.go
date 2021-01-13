e1a4ee062e91b9772f4572b71322505c92b2e949
Fuad Fadhil Azzar <30482903+fadhil1106@users.noreply.github.com>
2021-01-13 18:09:19 +0800

    finished essay

--- soalAlgoritma_1.go
+++ soalAlgoritma_1.go
@@ -3,18 +3,16 @@ package main
 import "fmt"
 
 func main() {
-   var number int = 7
-   var row int
-   var col int
+	number := 7
 
-   for row = 0 ; row < number ; row++{
-      for col = 0 ; col < number-row ; col++{
-         if(col == 0){
-            fmt.Print(row+1)
-         }else{
-            fmt.Print(0)
-         }
-      }
-      fmt.Println()
-   }
+	for row := 0; row < number; row++ {
+		for col := 0; col < number-row; col++ {
+			if col == 0 {
+				fmt.Print(row + 1)
+			} else {
+				fmt.Print(0)
+			}
+		}
+		fmt.Println()
+	}
 }
